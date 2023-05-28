c9561842de60fdb682316fa68d3ff2f26231ff43
Agemiro <agemiroalves11@gmail.com>
2023-05-27 22:38:28 -0300

    Add project files.

---
+++ PBSportStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PBSportStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
