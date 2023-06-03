89629bab3783d07283f7b8d20859d6cbff77c293
Agemiro <agemiroalves11@gmail.com>
2023-06-02 21:02:06 -0300

    refactor: Changed values for popular product table in migration

--- PBSportStore/Migrations/20230601014752_PopularProducts.cs
+++ PBSportStore/Migrations/20230601014752_PopularProducts.cs
@@ -15,19 +15,19 @@ namespace PBSportStore.Migrations
                 "VALUES('Camisa Sousa Esporte Clube', 'Descrição curta da camisa', 'Descrição detalhada da camisa'," +
                 "'179.99', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpJEQ-H4IAXFz19pHCXWKkFcPdJIba_p1-ZQ&usqp=CAU', " +
                 "'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpJEQ-H4IAXFz19pHCXWKkFcPdJIba_p1-ZQ&usqp=CAU', " +
-                "'1', '1', '2')");
+                "'1', '1', '1')");
             migrationBuilder.Sql("INSERT INTO Products(Name, ShortDescription, DetailedDescription, Price, " +
                 "ImageUrl, ImageThumbnailUrl, IsFavoriteProduct, InStock, CategoryId)" +
-                "VALUES('Camisa Campinense Clube', 'Descrição curta da camisa', 'Descrição detalhada da camisa'," +
-                "'149.99', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpJEQ-H4IAXFz19pHCXWKkFcPdJIba_p1-ZQ&usqp=CAU', " +
-                "'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpJEQ-H4IAXFz19pHCXWKkFcPdJIba_p1-ZQ&usqp=CAU', " +
-                "'0', '1', '3')");
+                "VALUES('Camisa Nacional de Patos', 'Descrição curta da camisa', 'Descrição detalhada da camisa'," +
+                "'174.99', 'https://static.netshoes.com.br/produtos/camisa-nacional-de-patos-i-2021-robrac-paraiba-paraibano/60/72S-0001-060/72S-0001-060_zoom1.jpg?ts=1640034026', " +
+                "'https://static.netshoes.com.br/produtos/camisa-nacional-de-patos-i-2021-robrac-paraiba-paraibano/60/72S-0001-060/72S-0001-060_zoom1.jpg?ts=1640034026', " +
+                "'0', '1', '1')");
             migrationBuilder.Sql("INSERT INTO Products(Name, ShortDescription, DetailedDescription, Price, " +
                 "ImageUrl, ImageThumbnailUrl, IsFavoriteProduct, InStock, CategoryId)" +
-                "VALUES('Atlético de Cajazeiras', 'Descrição curta da camisa', 'Descrição detalhada da camisa'," +
-                "'177.99', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpJEQ-H4IAXFz19pHCXWKkFcPdJIba_p1-ZQ&usqp=CAU', " +
-                "'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpJEQ-H4IAXFz19pHCXWKkFcPdJIba_p1-ZQ&usqp=CAU', " +
-                "'0', '1', '2')");
+                "VALUES('Camisa Atlético de Cajazeiras', 'Descrição curta da camisa', 'Descrição detalhada da camisa'," +
+                "'177.99', 'https://1.bp.blogspot.com/-KRMeQW4g-oc/YLeHKN8AZWI/AAAAAAAB0tA/whErG52pBGElPRFO0TsDDKlWRQiDtXuegCLcBGAsYHQ/s851/cajazeiras%2B%25281%2529.jpg', " +
+                "'https://1.bp.blogspot.com/-KRMeQW4g-oc/YLeHKN8AZWI/AAAAAAAB0tA/whErG52pBGElPRFO0TsDDKlWRQiDtXuegCLcBGAsYHQ/s851/cajazeiras%2B%25281%2529.jpg', " +
+                "'0', '1', '1')");
         }
 
         /// <inheritdoc />
